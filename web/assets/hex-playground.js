ec497d3f7842475fb8c5798c032426324424286e
h3rald <h3rald@h3rald.com>
2024-12-21 19:20:57 +0000

    Added textarea.

--- web/assets/hex-playground.js
+++ web/assets/hex-playground.js
@@ -1,6 +1,7 @@
 Module.pending_fgets = [];
 Module.pending_lines = [];
-const inputBox = document.querySelector("article input");
+const inputBox = document.querySelector("article textarea");
+const btn = document.querySelector("article button");
 const outputBox = document.querySelector("article section section");
 Module.print = (text) => {
   outputBox.textContent += text + "\n";
@@ -10,16 +11,13 @@ Module.printErr = (text) => {
   outputBox.textContent += text + "\n";
 };
 
-
-inputBox.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
+btn.addEventListener("click", () => {
     Module.pending_lines.push(inputBox.value);
     outputBox.textContent += "> " + inputBox.value + "\n";
     inputBox.value = '';
-  } else if (e.key.length === 1) {
-    Module.pending_chars.push(e.key);
-  }
+});
+
+inputBox.addEventListener("keydown", (e) => {
   if (Module.pending_fgets.length > 0 && Module.pending_lines.length > 0) {
     let resolver = Module.pending_fgets.shift();
     resolver(Module.pending_lines.shift());
